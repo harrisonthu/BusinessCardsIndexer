1707775159112ce039eff36eb588f1affcef716e
rxl79 <rxl7906@rit.edu>
2016-11-12 18:00:50 -0500

    Added camera function, needs testing

--- app/src/main/java/com/example/han/businesscardsindexer/MainActivity.java
+++ app/src/main/java/com/example/han/businesscardsindexer/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.han.businesscardsindexer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    public static final String ALLOW_KEY = "ALLOWED";
+    public static final String CAMERA_PREF = "camera_pref";
+
+    Button takePicture;
+    Button viewCards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find the elements
+        takePicture = (Button) findViewById(R.id.buttonTakePicture);
+        viewCards = (Button) findViewById(R.id.buttonViewCards);
+
+        // set a listener
+        takePicture.setOnClickListener(this);
+        viewCards.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+
+        openCamera();
+
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
     }
 }
