788b3847d1d44a9e468d1804d92721a00811412b
rxl79 <rxl7906@rit.edu>
2016-11-12 21:12:34 -0500

    Added camera function, needs more testing

--- app/src/main/java/com/example/han/businesscardsindexer/MainActivity.java
+++ app/src/main/java/com/example/han/businesscardsindexer/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.han.businesscardsindexer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.io.File;
+
+public class MainActivity extends Activity {
 
     public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
     public static final String ALLOW_KEY = "ALLOWED";
@@ -15,6 +21,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     Button takePicture;
     Button viewCards;
+    ImageView imageView;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +32,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // find the elements
         takePicture = (Button) findViewById(R.id.buttonTakePicture);
         viewCards = (Button) findViewById(R.id.buttonViewCards);
+        imageView = (ImageView) findViewById(R.id.image_view);
 
         // set a listener
-        takePicture.setOnClickListener(this);
-        viewCards.setOnClickListener(this);
+        takePicture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent, CAM_REQUEST);
+            }
+        });
+    }
+
+    private File getFile(){
+        File folder = new File("sdcard/camera_app");
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+        File image_file = new File(folder,"cam_image.jpg");
+        return image_file;
     }
 
     @Override
-    public void onClick(View v) {
-        //Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-
-        openCamera();
-
-    }
-
-    private void openCamera() {
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        String path = "sdcard/camera_app/cam_img.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
     }
 }
