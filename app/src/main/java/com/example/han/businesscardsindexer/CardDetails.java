16230d142260058062a0362fabd87540e549a661
Joseph Ville <jmv4610@rit.edu>
2016-11-13 01:07:34 -0500

    Hopefully commiting card stuff

--- app/src/main/java/com/example/han/businesscardsindexer/CardDetails.java
+++ app/src/main/java/com/example/han/businesscardsindexer/CardDetails.java
@@ -9,5 +9,6 @@ public class CardDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_details);
+        //comment
     }
 }
