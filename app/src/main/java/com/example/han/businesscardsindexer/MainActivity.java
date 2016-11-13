93077e010a98b52ca03e10c051bc62ccccb66dba
rxl79 <rxl7906@rit.edu>
2016-11-12 23:39:23 -0500

    Stable Version, saving to file system needs to be implemented

--- app/src/main/java/com/example/han/businesscardsindexer/MainActivity.java
+++ app/src/main/java/com/example/han/businesscardsindexer/MainActivity.java
@@ -11,6 +11,7 @@ import android.provider.MediaStore;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,7 +22,7 @@ import java.util.Date;
 
 public class MainActivity extends Activity {
 
-    /*public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
     public static final String ALLOW_KEY = "ALLOWED";
     public static final String CAMERA_PREF = "camera_pref";
 
@@ -29,6 +30,8 @@ public class MainActivity extends Activity {
     Button viewCards;
     ImageView imageView;
     static final int CAM_REQUEST = 1;
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,15 +62,16 @@ public class MainActivity extends Activity {
         }
         File image_file = new File(folder,"cam_image.jpg");
         return image_file;
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         String path = "sdcard/camera_app/cam_img.jpg";
         imageView.setImageDrawable(Drawable.createFromPath(path));
-    }*/
+    }
 
-    private Button takePictureButton;
+    /*private Button takePictureButton;
     private ImageView imageView;
     Uri file;
 
@@ -97,7 +101,7 @@ public class MainActivity extends Activity {
 
     public void takePicture(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        file = Uri.fromFile(getOutputMediaFile());
+        file = Uri.fromFile(getOutputMediaFile(1));
         intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
 
         startActivityForResult(intent, 100);
@@ -110,9 +114,9 @@ public class MainActivity extends Activity {
                 imageView.setImageURI(file);
             }
         }
-    }
+    }*/
 
-    private static File getOutputMediaFile(){
+    /*private static File getOutputMediaFile(){
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "CameraDemo");
 
@@ -126,5 +130,43 @@ public class MainActivity extends Activity {
         return new File(mediaStorageDir.getPath() + File.separator +
                 "IMG_"+ timeStamp + ".jpg");
     }
-    
+
+    /*public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    *//** Create a File for saving an image or video *//*
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }*/
+
 }
